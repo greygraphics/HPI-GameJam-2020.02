7843ae38f06fc2da2dd00932b2df565fe476f6bc
richardschiemenz <richard.schiemenz@student.hpi.de>
2020-02-29 15:32:07 +0100

    Make dog carry torch

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -2,10 +2,14 @@ extends KinematicBody
 
 export(float) var SPEED = 1
 
+export(NodePath) var CAMERA
+var camera : Camera
 var velocity = Vector3()
+var mousePos: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	camera = get_node_or_null(CAMERA)
 	pass # Replace with function body.
 
 
@@ -16,3 +20,14 @@ func _process(delta):
 	movement.z += Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	movement *= SPEED
 	velocity = move_and_slide(movement,Vector3.UP)
+	
+	#character rotation
+	
+	var dropPlane = Plane(Vector3.UP,translation.y)
+	var pos3d = dropPlane.intersects_ray(
+		camera.project_ray_origin(mousePos),camera.project_ray_normal(mousePos))
+	look_at(pos3d, Vector3.UP)
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		mousePos = event.position
