a9a8dc39b49b4a3e4f70214204ceb73dadeb5a72
greygraphics <philk.7438@gmail.com>
2020-02-29 12:53:48 +0100

    Remove project.godot

--- Scripts/Torch.gd
+++ Scripts/Torch.gd
@@ -1,16 +1,17 @@
 extends Spatial
 
+export(NodePath) var CAMERA
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var camera: Camera
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	camera = get_node_or_null(CAMERA)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var dropPlane = Plane(Vector3.UP,translation.y)
+	var mouseMos = dropPlane.intersects_ray(
+		camera.project_ray_origin(
