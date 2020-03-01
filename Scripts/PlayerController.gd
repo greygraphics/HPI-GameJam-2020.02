18c1ff33ec47c431b5eec275bbc69d869837e384
shieka <calli@email.com>
2020-03-01 10:50:03 +0100

    fancy Flashlight button yeahh

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -6,9 +6,11 @@ export(NodePath) var CAMERA
 var camera : Camera
 var velocity = Vector3()
 var mousePos: Vector2
+var torch
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	torch = $Torch
 	camera = get_node_or_null(CAMERA)
 	pass # Replace with function body.
 
@@ -27,6 +29,14 @@ func _process(delta):
 	var pos3d = dropPlane.intersects_ray(
 		camera.project_ray_origin(mousePos),camera.project_ray_normal(mousePos))
 	look_at(pos3d, Vector3.UP)
+	
+	#flashlight on/off
+	if Input.is_action_just_pressed("torch"):
+		if torch.is_visible_in_tree():
+			torch.hide()
+		else:
+			torch.show()
+	
 	
 func _input(event):
 	if event is InputEventMouseMotion:
