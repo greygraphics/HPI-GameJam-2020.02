2590fb33005a532dacd8b87899dcbe9b5d9c2211
greygraphics <philk.7438@gmail.com>
2020-03-01 14:54:49 +0100

    Remove useless cone check in torch

--- Scripts/Torch.gd
+++ Scripts/Torch.gd
@@ -16,16 +16,6 @@ func _process(delta):
 func _physics_process(delta):
 	if !on: 
 		return
-	# Test if other player is in cone
-	var my_global_translation = to_global(translation)
-	my_global_translation.y = 0
-	var target_global_translation = to_global(target.translation)
-	target_global_translation.y = 0
-	var direction = to_local(my_global_translation.direction_to(target_global_translation)).normalized()
-	var dot = Vector3.FORWARD.dot(direction)
-#	if dot < 0.9:
-#		return
-		
 	var space_state = get_world().direct_space_state
 	var result = space_state.intersect_ray(to_global(translation),to_global(Vector3.FORWARD * 100))
 	if result:
