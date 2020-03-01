9216311b254de296758f556bad51015beabd33d7
greygraphics <philk.7438@gmail.com>
2020-03-01 13:41:45 +0100

    Add cone detection

--- Scripts/Torch.gd
+++ Scripts/Torch.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 var target: Spatial
+var in_sight = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,10 +16,15 @@ func _physics_process(delta):
 	# Test if other player is in cone
 	var my_global_translation = to_global(translation)
 	var target_global_translation = to_global(target.translation)
-	var direction = to_local(my_global_translation.direction_to(target_global_translation))
+	var direction = to_local(my_global_translation.direction_to(target_global_translation)).normalized()
 	var dot = Vector3.FORWARD.dot(direction)
-	var angle = acos(dot)
-	if abs(angle) < 25:
-		print(target.name + " is in sight!")
+	if dot > 0.9:
+		if !in_sight:
+			print(target.name + " is in sight!")
+			in_sight = true;
+	else:
+		if in_sight:
+			print(target.name + " is not in sight!")
+			in_sight = false;
 	
 	pass
