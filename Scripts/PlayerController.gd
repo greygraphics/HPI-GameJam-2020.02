2296a368c2d32c6d46297e1ff3ec7ca5dddeb94e
greygraphics <philk.7438@gmail.com>
2020-03-01 14:32:20 +0100

    Add hit on one player

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -2,12 +2,10 @@ extends KinematicBody
 
 export(float) var SPEED = 10
 
-export(NodePath) var CAMERA
 export(NodePath) var WORLD
 export(Material) var DOG_MAT
 export(NodePath) var OTHER_PLAYER
 
-var camera : Camera
 var velocity = Vector3()
 var mousePos: Vector2
 var torch
@@ -21,7 +19,6 @@ var joy_name = ""
 func _ready():
 	torch = $Torch
 	torch.target = get_node_or_null(OTHER_PLAYER)
-	camera = get_node_or_null(CAMERA)
 	splatterer.world = get_node_or_null(WORLD)
 	joy_name = "joy" + str(JOY_INDEX)
 	$MeshInstance.material_override = DOG_MAT
@@ -60,6 +57,5 @@ func look(delta):
 	if abs(hor) > DEADZONE or abs(ver) > DEADZONE:
 		rotation_degrees.y = rad2deg(rot)
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		mousePos = event.position
+func _is_hit():
+	splatterer._is_hit()
--- Scripts/Torch.gd
+++ Scripts/Torch.gd
@@ -19,12 +19,9 @@ func _physics_process(delta):
 	var direction = to_local(my_global_translation.direction_to(target_global_translation)).normalized()
 	var dot = Vector3.FORWARD.dot(direction)
 	if dot > 0.9:
-		if !in_sight:
-			print(target.name + " is in sight!")
-			in_sight = true;
-	else:
-		if in_sight:
-			print(target.name + " is not in sight!")
-			in_sight = false;
-	
-	pass
+		return
+		
+	var space_state = get_world().direct_space_state
+	var result = space_state.intersect_ray(to_global(translation),to_global(Vector3.FORWARD * 100))
+	if result:
+		result.collider._is_hit()
