4be33ffff7bfba01cdb927aeafb989ea43efeb0d
greygraphics <philk.7438@gmail.com>
2020-03-01 11:40:16 +0100

    Make player look by stick

--- Scripts/ControllerTest.gd
+++ Scripts/ControllerTest.gd
@@ -1,12 +1,25 @@
 extends Spatial
 
-export(int) var CONTROLLER_INDEX = 0
+export(int) var JOY_INDEX = 0
+export(float) var DEADZONE = 0.5
+var joy_name = ""
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	joy_name = "joy" + str(JOY_INDEX)
 
+func _process(delta):
+		move(delta)
+		look(delta)
+	
+func move(delta):
+	var hor = Input.get_action_strength(joy_name + "_right") - Input.get_action_strength(joy_name + "_left")
+	var ver = Input.get_action_strength(joy_name + "_down") - Input.get_action_strength(joy_name + "_up")
+	var movement = Vector3(hor,0,ver)
+	translation += movement * 4 * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func look(delta):
+	var hor = Input.get_action_strength(joy_name + "_look-right") - Input.get_action_strength(joy_name + "_look-left")
+	var ver = Input.get_action_strength(joy_name + "_look-up") - Input.get_action_strength(joy_name + "_look-down")
+	var rot = atan2(ver,hor)
+	if abs(hor) > DEADZONE or abs(ver) > DEADZONE:
+		rotation_degrees.y = rad2deg(rot)
